2d7f20cbb488d39831feea9d28c94a0d713bb754
malencioni <michael.lencioni@kellogg.northwestern.edu>
2020-05-17 03:03:37 +0000

    Error

--- app.rb
+++ app.rb
@@ -4,19 +4,19 @@ require "httparty"
 def view(template); erb template.to_sym; end
 
 get "/" do
-  ### Get the weather
-  # Evanston, Kellogg Global Hub... replace with a different location if you want
-  lat = 42.0574063
-  long = -87.6722787
+    lat = 42.0574063
+    long = -87.6722787
+    units = "imperial" # or metric, whatever you like
+    key = "7e94c93c86a480064398484949fe26a4" # replace this with your real OpenWeather API key
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
+    #url = "https://api.openweathermap.org/data/2.5/onecall?lat=42.0574063&lon=-87.6722787&units=imperial&appid=7e94c93c86a480064398484949fe26a4"
+    forecast = HTTParty.get(url).parsed_response.to_hash
 
-  units = "imperial" # or metric, whatever you like
-  key = "YOUR-API-KEY-GOES-HERE" # replace this with your real OpenWeather API key
-
-  # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
-  url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
-
-  # make the call
-  @forecast = HTTParty.get(url).parsed_response.to_hash
-
-  ### Get the news
+    puts "It is currently #{forecast["current"]["temp"]} degrees and #{forecast["current"]["weather"][0]["description"]}"
+    puts "Extended forecast:"
+day_number = 1
+for day in forecast["daily"]
+   puts "On day #{day_number}, A high of #{day["temp"]["max"]} and #{day["weather"][0]["description"]}"
+   day_number = day_number + 1
+end
 end
